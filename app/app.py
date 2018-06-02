b4e7626d3f788e935da15b41294720d4b05d8a81
Maegner <francisco.aguiar@tecnico.ulisboa.pt>
2018-06-02 17:47:15 +0100

    controller started, returning correct response based on request model and request name

---
+++ app/app.py
@@ -0,0 +1,41 @@
+from src.domain import model_manager
+from src.utils import name_parser
+from flask import Flask,abort,request
+
+manager = model_manager.Model_Manager()
+app = Flask(__name__)
+
+@app.route('/help',methods=['POST'])
+def help_request():
+    help = request.get_json()
+    
+    try:
+        help_id = help["id"]
+        help_request = help["request"]
+        
+        name = name_parser.get_names(help_request)
+        help_model = manager.get_model(help_id)
+        
+        if help_model == None:
+            abort(404)
+            return
+        
+        return help_model.get_response(name)
+    except KeyError:
+        abort(400)
+        return
+
+def settup(debug=True):
+    
+    global manager
+    manager
+    manager.retrieve_data()
+
+    if manager.is_empty():
+        manager.populate()
+        manager.store()
+
+    app.run(debug=debug)
+
+if __name__ == '__main__':
+    settup()
--- app/src/domain/model_manager.py
+++ app/src/domain/model_manager.py
@@ -20,6 +20,22 @@ class Model_Manager:
         except KeyError:
             result = None
         return result
+
+    def is_empty(self):
+        return len(self.current_models) == 0
+    
+    def populate(self):
+        
+        id_1 = "26444047-56f8-43e5-8e25-b70b8e5367f1"
+        response_1 = "Thanks for getting in touch {}! I will try my best to help."
+        model_1 = model.Model(id_1,response_1)
+        self.add_model(model_1)
+
+        id_2 = "f59da1c7-c060-4aec-b3a0-dd7453a4c541"
+        response_2 = "Hello {}! How can I help you?"
+        model_2 = model.Model(id_2,response_2)
+        self.add_model(model_2)
+
     
     def retrieve_data(self):
         try:
